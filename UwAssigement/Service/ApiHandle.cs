852d4bfcbd1afa33bceda7fa9bd783109d8be589
unknown <doanncd00530@fpt.edu.vn>
2018-10-25 15:47:47 +0700

    Bài Assigement môn UWP

---
+++ UwAssigement/Service/ApiHandle.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using UwAssigement.Entity;
+
+namespace UwAssigement.Service
+{
+    class ApiHandle
+    {
+        private static string API_URL = "https://2-dot-backup-server-002.appspot.com/_api/v2/members";
+        private static string API_LOGIN = "http://2-dot-backup-server-002.appspot.com/_api/v2/members/authentication";
+        private static string SONG_API = "http://2-dot-backup-server-002.appspot.com/_api/v2/songs";
+
+        public async static Task<HttpResponseMessage> Sign_Up(Member member)
+        {
+            HttpClient httpClient = new HttpClient();
+            var content = new StringContent(JsonConvert.SerializeObject(member), System.Text.Encoding.UTF8, "application/json");
+            var response = httpClient.PostAsync(API_URL, content);
+            return response.Result;
+        }
+
+        public async static Task<HttpResponseMessage> Sign_In(string email, string password)
+        {
+            Dictionary<String, String> LoginInfor = new Dictionary<string, string>();
+            LoginInfor.Add("email", email);
+            LoginInfor.Add("password", password);
+
+            HttpClient httpClient = new HttpClient();
+            StringContent content = new StringContent(JsonConvert.SerializeObject(LoginInfor), System.Text.Encoding.UTF8, "application/json");
+            var response = httpClient.PostAsync(API_LOGIN, content).Result;
+            return response;
+        }
+        public async static Task<string> Create_Song(Song song)
+        {
+            HttpClient httpClient = new HttpClient();
+            var content = new StringContent(JsonConvert.SerializeObject(song), System.Text.Encoding.UTF8, "application/json");
+            var response = httpClient.PostAsync(SONG_API, content);
+            var contents = await response.Result.Content.ReadAsStringAsync();
+            Debug.WriteLine(contents);
+            return contents;
+        }
+    }
+}
---
+++ UwAssigement/Views/AddSong.xaml.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UwAssigement.Entity;
+using UwAssigement.Service;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Content Dialog item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace UwAssigement.Views
+{
+    public sealed partial class AddSong : ContentDialog
+    {
+        private Song currentSong;
+
+        public AddSong()
+        {
+            this.currentSong = new Song();
+            this.InitializeComponent();
+        }
+
+        private void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+        }
+
+        private void ContentDialog_SecondaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+        }
+
+        private async void SaveSong(object sender, RoutedEventArgs e)
+        {
+            StorageFolder folder = ApplicationData.Current.LocalFolder;
+
+            StorageFile file = await folder.GetFileAsync("token.txt");
+            var tokenContent = await FileIO.ReadTextAsync(file);
+
+            TokenResponse token = JsonConvert.DeserializeObject<TokenResponse>(tokenContent);
+
+            this.currentSong.name = this.NameSong.Text;
+            this.currentSong.description = this.Description.Text;
+            this.currentSong.singer = this.Singer.Text;
+            this.currentSong.author = this.Author.Text;
+            this.currentSong.thumbnail = this.Thumbnail.Text;
+            this.currentSong.link = this.Link.Text;
+
+            await ApiHandle.Create_Song(this.currentSong);
+            Debug.WriteLine("Action success.");
+        }
+
+        private void BtnReset(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
